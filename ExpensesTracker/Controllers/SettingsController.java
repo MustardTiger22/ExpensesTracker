0a34f022dcd7c72c8501adf1f94950825c857c1f
Dominik <Ramirez459@gmail.com>
2019-03-12 19:17:16 +0100

    Logic of the Settings

--- ExpensesTracker/Controllers/SettingsController.java
+++ ExpensesTracker/Controllers/SettingsController.java
@@ -4,22 +4,42 @@ import ExpensesTracker.Models.Settings;
 import com.gluonhq.charm.glisten.control.TextField;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class SettingsController implements Initializable {
-    private Settings model = new Settings();
+    private Settings model;
 
     @FXML private TextField budget;
     @FXML private TextField expenses;
     @FXML private TextField bills;
     @FXML private TextField accounts;
     @FXML private TextField income;
+    @FXML private Button saveBtn;
+
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        try {
+            if(model.IfTheFileExists()) {
+                budget = new TextField(model.getBudget());
+                expenses = new TextField(model.getExpenses());
+                bills = new TextField(model.getBills());
+                accounts = new TextField(model.getAccounts());
+                income = new TextField(model.getIncome());
+            }
+        }
+        catch (NullPointerException e)
+        {
+            e.printStackTrace();
+        }
 
+        saveBtn.setOnAction(e -> {
+            model = new Settings(budget.toString(), expenses.toString(), bills.toString(), accounts.toString(), income.toString());
+            model.SaveSettings();
+        });
     }
 
 
--- ExpensesTracker/Models/Settings.java
+++ ExpensesTracker/Models/Settings.java
@@ -1,5 +1,84 @@
 package ExpensesTracker.Models;
 
-public class Settings {
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
+public class Settings implements Serializable {
+    private String budget;
+    private String expenses;
+    private String bills;
+    private String accounts;
+    private String income;
+
+    public Settings(String budget, String expenses, String bills, String accounts, String income) {
+        this.budget = budget;
+        this.expenses = expenses;
+        this.bills = bills;
+        this.accounts = accounts;
+        this.income = income;
+    }
+
+    public String getBudget() {
+        return budget;
+    }
+    public String getExpenses() {
+        return expenses;
+    }
+    public String getBills() {
+        return bills;
+    }
+    public String getAccounts() {
+        return accounts;
+    }
+    public String getIncome() {
+        return income;
+    }
+
+
+    public Settings() {
+
+    }
+
+    public void SaveSettings() {
+        try {
+            FileOutputStream fs = new FileOutputStream("Configuration/usersettings.ser");
+            ObjectOutputStream os = new ObjectOutputStream(fs);
+            os.writeObject(this);
+            os.close();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Settings LoadSettings() {
+
+        try {
+            FileInputStream fs = new FileInputStream("Configuration/usersettings.ser");
+            ObjectInputStream is = new ObjectInputStream(fs);
+            Settings oneRestore = (Settings) is.readObject();
+            is.close();
+            return this;
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static Boolean IfTheFileExists(){
+
+        try {
+            final Path path = Files.createTempFile("testFile", ".txt");
+            if(Files.exists(path))
+                return true;
+            else
+                return false;
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
