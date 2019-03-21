ee66eef6fd1039e7085f986cb8446271ef047966
Dominik <ramirez459@gmail.com>
2019-03-21 23:08:15 +0100

    Improved GUI

--- ExpensesTracker/Controllers/DashboardController.java
+++ ExpensesTracker/Controllers/DashboardController.java
@@ -5,9 +5,17 @@ import ExpensesTracker.Models.Dashboard;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.chart.PieChart;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -17,6 +25,8 @@ public class DashboardController implements Initializable {
     private Dashboard model = new Dashboard();
     private Settings userSettings;
 
+    @FXML private Button showExpensesBoardBtn;
+    @FXML private Button addExpenseBtn;
     @FXML private PieChart recapPieChart;
     @FXML private Label dateValue;
     @FXML private Label username;
@@ -29,12 +39,48 @@ public class DashboardController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        userSettings = new Settings();
-        userSettings.LoadSettings(username, budget, expenses, bills, accounts, income);
-
+        if(userSettings.IfTheFileExists()) {
+            userSettings = new Settings();
+            userSettings.LoadSettings(username, budget, expenses, bills, accounts, income);
+        }
         dateValue.setText(model.getFormattedDate());
 
+        //Buttons
+        showExpensesBoardBtn.setOnAction(e -> {
+            try {
+                //Switch scenes
+                Stage window = new Stage();
+                Parent root = FXMLLoader.load(getClass().getResource("../Views/ExpensesboardUI.fxml"));
+                window.setTitle("Expenses Tracker - board");
+                window.setScene(new Scene(root));
+                window.show();
+                Node source = (Node) e.getSource();
+                Stage stage = (Stage) source.getScene().getWindow();
+                stage.close();
+            }
+            catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        });
+        addExpenseBtn.setOnAction(e -> {
+            try {
+                //Switch scenes
+                Stage window = new Stage();
+                Parent root = FXMLLoader.load(getClass().getResource("../Views/AddExpenseUI.fxml"));
+                window.setTitle("Expenses Tracker - board");
+                window.setScene(new Scene(root));
+                window.show();
+                Node source = (Node) e.getSource();
+                Stage stage = (Stage) source.getScene().getWindow();
+                stage.close();
+            }
+            catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        });
 
+
+        //Pie chart
         ObservableList<PieChart.Data> pieChartData =
                 FXCollections.observableArrayList(
                         new PieChart.Data("Expenses", Double.parseDouble(expenses.getText())),
@@ -44,6 +90,6 @@ public class DashboardController implements Initializable {
                         new PieChart.Data("Bills", Double.parseDouble(bills.getText())));
         recapPieChart.setData(pieChartData);
 
-        dateValue.setText(model.getFormattedDate());
+
     }
 }
